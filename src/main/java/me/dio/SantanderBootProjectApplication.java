3c8503b3c0ff2d88f790aa860c895239b820a62c
Mateus Henrique Derossi <henriquemateus2502@gmail.com>
2023-10-13 17:07:16 -0300

    Initial commit

---
+++ src/main/java/me/dio/SantanderBootProjectApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootProjectApplication.class, args);
+	}
+
+}
