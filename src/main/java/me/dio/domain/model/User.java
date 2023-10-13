78d08d54c605cd080229374827476b876087387a
Mateus Henrique Derossi <henriquemateus2502@gmail.com>
2023-10-13 20:03:10 -0300

    service-add

--- src/main/java/me/dio/domain/model/User.java
+++ src/main/java/me/dio/domain/model/User.java
@@ -30,6 +30,18 @@ public class User {
 		return account;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setFeatures(List<Feature> features) {
+		this.features = features;
+	}
+
 	public void setAccount(Account account) {
 		this.account = account;
 	}
