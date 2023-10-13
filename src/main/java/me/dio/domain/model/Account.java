30a518d965fc0c40f10b27d268f9caeac9b5a10c
Mateus Henrique Derossi <henriquemateus2502@gmail.com>
2023-10-13 19:44:09 -0300

    hotfix-precision-scale

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -14,7 +14,7 @@ public class Account {
 	private String agency;
 	@Column(precision = 2, scale = 13)
 	private BigDecimal balance;
-	@Column(name = "additional_limit",precision = 2, scale = 13)
+	@Column(name = "additional_limit",precision = 13, scale = 2)
 	private BigDecimal limit;
 
 	public Long getId() {
