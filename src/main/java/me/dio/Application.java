1f9ae84aac205b3fea53f04a93d435f4c9284048
Mateus Henrique Derossi <henriquemateus2502@gmail.com>
2023-10-17 16:29:57 -0300

    controller add

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,13 +1,13 @@
-package me.dio.domain;
+package me.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderBootProjectApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderBootProjectApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
